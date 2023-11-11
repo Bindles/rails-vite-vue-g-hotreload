b2741b1dc977f5cc01e752ab5ba51f8bbe85b9e8
= <=>
2023-11-11 08:01:10 -0500

    c2-added-component-counter

---
+++ app/javascript/components/App.vue
@@ -0,0 +1,13 @@
+<template lang="">
+  <div>
+    <h1>Vue</h1>
+    <Counter />
+  </div>
+</template>
+<script>
+import Counter from "./Counter.vue";
+export default {
+  components: { Counter },
+};
+</script>
+<style lang=""></style>
---
+++ app/javascript/components/Counter.vue
@@ -0,0 +1,24 @@
+<template lang="">
+  <div>
+    <button @click="increment" id="counter-button">Increment</button>
+    <p>Count is: {{ count }}</p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return { count: 0 };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+  },
+};
+</script>
+<style lang="css">
+#button {
+    background-color: red;
+}
+}
+</style>
